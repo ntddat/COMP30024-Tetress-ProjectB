df899767cd33bfcfd8029aac1c8e1156d3edc74e
ntddat <157578487+ntddat@users.noreply.github.com>
2024-05-01 17:00:49 +1000

    Add files via upload

---
+++ agentv3/zobrist.py
@@ -0,0 +1,15 @@
+# COMP30024 Artificial Intelligence, Semester 1 2024
+# Project Part B: Single Player Tetress
+import numpy as np
+import random
+
+SEED = 1370085
+
+class Zobrist:
+
+    """
+    121*2 random numbers
+    look at the seblague and the blunder bot
+    """
+
+    def _init_(self):
